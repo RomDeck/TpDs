b3380b0568619707d70e9f7b522f92ee3eebbc5f
RomDeck <152853318+RomDeck@users.noreply.github.com>
2024-08-22 14:48:31 +0200

    Création du menu
    Scene Credits créée

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Fonction pour lancer le jeu
+    public void PlayGame()
+    {
+        // Charger la scène "Game"
+        SceneManager.LoadScene("Game");
+    }
+
+    // Fonction pour afficher les crédits
+    public void ShowCredits()
+    {
+        // Charger la scène "Credits"
+        SceneManager.LoadScene("Credits");
+    }
+
+    // Fonction pour quitter le jeu
+    public void QuitGame()
+    {
+        // Si on est en mode éditeur, arrêter le mode de jeu
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // Si on est en build, quitter l'application
+            Application.Quit();
+#endif
+    }
+}
